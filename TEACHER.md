0fd994534bccbe7a4cc8d0dcfbe948c8acad4069
tabatha wiggers <tatisabato@hotmail.com>
2020-08-22 08:20:09 -0300

    seeds 100 restaurants

---
+++ TEACHER.md
@@ -0,0 +1,19 @@
+## SET UP -> follow slides
+
+## CREATES MODELS -> design database
+Restaurants
+id
+name
+rating
+address
+
+## GENERATE MODEL:
+rails g model restaurant name address rating:integer
+FILES GENERATED:
+- migration file create restaurants
+- restaurant model
+- routes -> let's code!!!
+- restaurants controller
+
+SEQUENCE:
+
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurants = Restaurant.new
+  end
+
+  def edit
+  end
+
+  def create
+    @restaurants = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created!'
+    else
+      render :new
+  end
 end
